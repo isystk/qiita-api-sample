053f1b81ebbf1fd4ffa12ac3dbd7537ea1b4866b
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-04-09 14:09:49 +0900

    コンテキストパスを指定しても問題ないように修正

---
+++ src/pages/top/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Top></Top>
+</template>
+
+<script>
+import Top from '../../components/Top.vue';
+
+export default {
+  components: {
+    Top
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+}
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Top from '../components/Top'
+import Top from '../pages/top'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
